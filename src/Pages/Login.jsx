a883174702ca9ac4ea640edfe03fce9cafe8174e
Bloodcrane <92672973+Bloodcrane@users.noreply.github.com>
2025-02-25 06:16:49 +0400

    style

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import supabase from '../supabase';
+import "./LogInUp.css"
 
 const Login = () => {
   const [email, setEmail] = useState('');
